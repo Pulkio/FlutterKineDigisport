1478eb1160f3ca25bbdd19673b78be9c61ff95bc
Pulkio <guillaume.cobat@gmx.fr>
2023-11-23 12:02:15 +0100

    Importer video

--- ReactKineApp/app/screens/Test.tsx
+++ ReactKineApp/app/screens/Test.tsx
@@ -1,93 +1,115 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import * as ImagePicker from 'expo-image-picker';
 import { FIRESTORE_DB } from '../../FirebaseConfig';
 import { getDocs, collection } from 'firebase/firestore';
+import { getAuth, User } from 'firebase/auth';
 
-const TestComponent = () => {
-    const [selectedTest, setSelectedTest] = useState('');
-    const [selectedPatient, setSelectedPatient] = useState('');
-    const [patients, setPatients] = useState<{ value: string; label: string }[]>([]);
-  
-    useEffect(() => {
-      const fetchPatientsFromDatabase = async () => {
-        try {
-          const snapshot = await getDocs(collection(FIRESTORE_DB, 'Patient'));
-          const patientList = snapshot.docs.map((doc) => {
-            const { nom, prenom } = doc.data();
-            return {
-              value: `${prenom} ${nom}`,
-              label: `${prenom} ${nom}`,
-            };
+const Test = () => {
+  const [selectedTest, setSelectedTest] = useState<string | null>(null);
+  const [selectedPatient, setSelectedPatient] = useState<string | null>(null);
+  const [tests, setTests] = useState<string[]>([]);
+  const [patients, setPatients] = useState<string[]>([]);
+  const currentUser = getAuth().currentUser;
+
+  useEffect(() => {
+    // Chargement des tests (exemple : "Test1", "Test2", "Test3", "Test4")
+    setTests(["Test1", "Test2", "Test3", "Test4"]);
+
+    // Chargement des patients de l'utilisateur connecté depuis la base de données
+    const fetchPatients = async () => {
+      try {
+        if (currentUser) {
+          const querySnapshot = await getDocs(collection(FIRESTORE_DB, `users/${currentUser.uid}/patients`));
+          const patientsData: string[] = [];
+          querySnapshot.forEach((doc) => {
+            patientsData.push(doc.id);
           });
-          setPatients(patientList);
-        } catch (error) {
-          console.error('Error fetching patients:', error);
+          setPatients(patientsData);
         }
-      };
-  
-      fetchPatientsFromDatabase();
-    }, []);
-  
-    const handleStartTest = () => {
-      console.log('Starting test:', selectedTest, 'for patient:', selectedPatient);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des patients : ', error);
+      }
     };
-  
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>Sélectionnez le test :</Text>
-        <Picker
-          selectedValue={selectedTest}
-          onValueChange={(itemValue) => setSelectedTest(itemValue)}
-          style={styles.picker}
-        >
-          <Picker.Item label="Test1" value="Test1" />
-          <Picker.Item label="Test2" value="Test2" />
-          <Picker.Item label="Test3" value="Test3" />
-          <Picker.Item label="Test4" value="Test4" />
-        </Picker>
-  
-        <Text style={styles.label}>Sélectionnez le patient :</Text>
-        <Picker
-          selectedValue={selectedPatient}
-          onValueChange={(itemValue) => setSelectedPatient(itemValue)}
-          style={styles.picker}
-        >
-          {patients.map((patient, index) => (
-            <Picker.Item key={index.toString()} label={patient.label} value={patient.value} />
-          ))}
-        </Picker>
-  
-        <TouchableOpacity style={styles.button} onPress={handleStartTest}>
-          <Text style={styles.buttonText}>Commencer le Test</Text>
-        </TouchableOpacity>
-      </View>
-    );
+
+    fetchPatients();
+  }, [currentUser]);
+
+  const importVideos = async () => {
+    try {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.error('Permission refusée pour accéder à la bibliothèque de médias.');
+        return;
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      });
+
+      if (!result.canceled) {
+        console.log('Vidéo sélectionnée : ', result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'importation des vidéos : ', error);
+    }
   };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Choisir un Test</Text>
+
+      {/* Liste déroulante pour choisir un test */}
+      <Picker
+        selectedValue={selectedTest}
+        onValueChange={(itemValue) => setSelectedTest(itemValue)}
+      >
+        <Picker.Item label="Sélectionner un test" value={null} />
+        {tests.map((test, index) => (
+          <Picker.Item key={index} label={test} value={test} />
+        ))}
+      </Picker>
+
+      <Text style={styles.title}>Choisir un Patient</Text>
+
+      {/* Liste déroulante pour choisir un patient */}
+      <Picker
+        selectedValue={selectedPatient}
+        onValueChange={(itemValue) => setSelectedPatient(itemValue)}
+      >
+        <Picker.Item label="Sélectionner un patient" value={null} />
+        {patients.map((patient, index) => (
+          <Picker.Item key={index} label={patient} value={patient} />
+        ))}
+      </Picker>
+
+      {/* Bouton pour importer des vidéos */}
+      <TouchableOpacity style={styles.button} onPress={importVideos}>
+        <Text style={styles.buttonText}>Importer des Vidéos</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: '#fff',
-    justifyContent: 'center', // Center vertically
+    padding: 20,
   },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  picker: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    marginBottom: 15,
-    paddingHorizontal: 10,
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
   button: {
     backgroundColor: '#007BFF',
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
+    marginTop: 20,
   },
   buttonText: {
     color: '#fff',
@@ -95,4 +117,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default TestComponent;
+export default Test;
